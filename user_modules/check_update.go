f1b4e9c734890059fe02fdb86df3ff124b66cf27
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2024-08-27 10:06:04 -0700

    Update check_update.go

--- user_modules/check_update.go
+++ user_modules/check_update.go
@@ -9,55 +9,55 @@ import (
 
 func CheckUpdate(appVersion string) {
 	defer updateWG.Done()
-
+	
 	type GitResponse struct {
 		TagName    string `json:"tag_name"`
 		ReleaseUrl string `json:"html_url"`
 	}
-
+	
 	var (
 		apiResponse GitResponse
 		err         error
 		resp        *http.Response
 	)
-
+	
 	httpClient := &http.Client{Timeout: 5 * time.Second}
-
+	
 	resp, err = httpClient.Get("https://api.github.com/repos/Underneach/Nicotine-String-Sorter/releases/latest")
 	if err != nil {
 		WaitLogo()
 		PrintErr()
-		fmt.Print("Не удалось проверить обновления : ", err, "\n\n")
+		fmt.Print("Failed to check updates: ", err, "\n\n")
 		return
 	}
-
+	
 	if resp.StatusCode != 200 {
 		WaitLogo()
 		PrintErr()
-		fmt.Print("Не удалось проверить обновления : Код HTTP ", resp.StatusCode, "\n\n")
+		fmt.Print("Failed to check updates: HTTP code ", resp.StatusCode, "\n\n")
 		return
 	}
-
+	
 	if err = json.NewDecoder(resp.Body).Decode(&apiResponse); err != nil {
 		WaitLogo()
 		PrintErr()
-		fmt.Print("Не удалось проверить обновления : ", err, "\n\n")
+		fmt.Print("Failed to check updates: ", err, "\n\n")
 		return
 	}
-
+	
 	if apiResponse.TagName != appVersion {
 		WaitLogo()
 		PrintSuccess()
-		fmt.Print("Доступна новая версия : ")
+		fmt.Print("New version available: ")
 		ColorBlue.Print(apiResponse.TagName, "\n")
 		PrintSuccess()
 		ColorBlue.Print(apiResponse.ReleaseUrl, "\n")
 		PrintSuccess()
-		fmt.Print("Скачайте новый .exe и замените текущий\n\n")
+		fmt.Print("Download the new .exe and replace the current one\n\n")
 	} else {
 		WaitLogo()
 		PrintSuccess()
-		fmt.Print("У вас последняя версия сортера\n\n")
+		fmt.Print("You have the latest version of the sorter\n\n")
 	}
 	_ = resp.Body.Close()
 }
