fb7478f8ff29d42d570cd0bb0bff1cbaf20f36f6
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2024-08-27 10:04:49 -0700

    Update variables.go

--- user_modules/variables.go
+++ user_modules/variables.go
@@ -11,18 +11,18 @@ import (
 
 var (
 
-	// ColorBlue Цвета
+	// Colors
 	ColorBlue    = color.New(color.FgBlue).Add(color.Bold)
 	ColorGreen   = color.New(color.FgGreen).Add(color.Bold)
 	ColorRed     = color.New(color.FgRed).Add(color.Bold)
 	ColorMagenta = color.New(color.FgMagenta).Add(color.Bold)
 	ColorYellow  = color.New(color.FgYellow).Add(color.Bold)
 
-	filesSize       int64                       // Размер всех входных файлов
-	userInputReader = bufio.NewReader(os.Stdin) // Альтернативный ридер инпута с поддержкой пробелов
-	userOs          = runtime.GOOS              // ОС юзера
-	updateWG        sync.WaitGroup              // ВГ обновы
-	isLogoPrinted   = false                     // Напечатано ли лого
+	filesSize       int64                       // Size of all input files
+	userInputReader = bufio.NewReader(os.Stdin) // Alternative input reader with space support
+	userOs          = runtime.GOOS              // User's OS
+	updateWG        sync.WaitGroup              // Update wait group
+	isLogoPrinted   = false                     // Is the logo printed
 
 	workMode       string
 	filePathList   []string
